4beb7d2ed8a5ccf0fec97887323b8e683cca03e9
SeungHwan Jeon <arror1784@gmail.com>
2020-06-22 18:24:52 +0900

    change config and install.sh

--- package/install.sh
+++ package/install.sh
@@ -5,6 +5,11 @@ TARGET_FOLDER="./targets"
 TARGET_IP=""
 TARGET_PASSWD=""
 
+SERVICES=(
+"capsuleReactService"
+"capsuleDjangoService"
+)
+
 ROOT_UPDATE_TARGET=(
 "capsuleFW"
 "capsuleSetting.json"
@@ -13,6 +18,8 @@ ROOT_UPDATE_TARGET=(
 "sshd_config"
 "lightdm.conf"
 "black_image.jpg"
+"capsuleReactService"
+"capsuleDjangoService"
 )
 ROOT_UPDATE_PATH=(
 "/opt/capsuleFW/bin/capsuleFW"
@@ -22,6 +29,8 @@ ROOT_UPDATE_PATH=(
 "/etc/ssh/sshd_config"
 "/etc/lightdm/lightdm.conf"
 "/usr/share/rpd-wallpaper/black_image.jpg"
+"/etc/init.d/capsuleReactService"
+"/etc/init.d/capsuleDjangoService"
 )
 
 PI_UPDATE_TARGET=(
@@ -42,14 +51,60 @@ fi
 TARGET_IP=$1
 TARGET_PASSWD=$2
 
+SSHPASS="sshpass -p ${TARGET_PASSWD}"
+
+SSH_COMMAND_PI="${SSHPASS} ssh pi@${TARGET_IP}"
+SSH_COMMAND_ROOT="${SSHPASS} ssh root@${TARGET_IP}"
+
+#capsuleFW process kill
+
+${SSHPASS} ssh pi@${TARGET_IP} pkill capsuleFW
+
 for (( i = 0 ; i < ${#ROOT_UPDATE_TARGET[@]} ; i++ )) ; do
 	echo ${ROOT_UPDATE_TARGET[$i]} ${ROOT_UPDATE_PATH[$i]}
-	sshpass -p "${TARGET_PASSWD}" scp ${TARGET_FOLDER}/${ROOT_UPDATE_TARGET[$i]} root@${TARGET_IP}:${ROOT_UPDATE_PATH[$i]}
+	${SSHPASS} scp ${TARGET_FOLDER}/${ROOT_UPDATE_TARGET[$i]} root@${TARGET_IP}:${ROOT_UPDATE_PATH[$i]}
 done
 
 for (( i = 0 ; i < ${#PI_UPDATE_TARGET[@]} ; i++ )) ; do
 	echo ${PI_UPDATE_TARGET[$i]} ${PI_UPDATE_PATH[$i]}
-	sshpass -p "${TARGET_PASSWD}" scp ${TARGET_FOLDER}/${PI_UPDATE_TARGET[$i]} pi@${TARGET_IP}:${PI_UPDATE_PATH[$i]}
+	${SSHPASS} scp ${TARGET_FOLDER}/${PI_UPDATE_TARGET[$i]} pi@${TARGET_IP}:${PI_UPDATE_PATH[$i]}
 done
+
+for (( i = 0 ; i < ${#SERVICES[@]} ; i++ )) ; do
+	${SSHPASS} ssh root@${TARGET_IP} chmod 755 /etc/init.d/${SERVICES[$i]}
+	${SSHPASS} ssh pi@${TARGET_IP} sudo update-rc.d ${SERVICES[$i]} defaults
+done
+
+#${SSH_COMMAND_ROOT} mkdir /opt/capsuleFW_react/ 2>&1 > /dev/null
+${SSH_COMMAND_ROOT} "rm -rf /opt/capsuleFW_react"
+${SSHPASS} scp -r ./capsuleFW_react root@${TARGET_IP}:/opt/
+
+#frontend
+#yarn installed
+
+if ${SSH_COMMAND_PI} type yarn 2> /dev/null; then
+	echo "yarn installed"
+else
+	echo "yarn install"
+	${SSH_COMMAND_PI} "curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -"
+	${SSH_COMMAND_PI} "echo \"deb https://dl.yarnpkg.com/debian/ stable main\" | sudo tee /etc/apt/sources.list.d/yarn.list"
+
+	${SSH_COMMAND_PI} sudo apt-get update -y
+	${SSH_COMMAND_PI} sudo apt-get install yarn -y
+
+	${SSH_COMMAND_PI} yarn global add serve
+fi
+
+#backend
+if ${SSH_COMMAND_PI} type redis-server 2> /dev/null; then
+	echo "redis-server installed"
+else
+	echo "redis-server install"
+	${SSH_COMMAND_PI} "sudo apt-get install redis-server -y"
+fi
+${SSH_COMMAND_PI} "sudo pip3 install -r /opt/capsuleFW_react/backend/requirements.txt"
+
+${SSH_COMMAND_PI} "sudo python3 /opt/capsuleFW_react/backend/manage.py makemigrations"
+${SSH_COMMAND_PI} "sudo python3 /opt/capsuleFW_react/backend/manage.py migrate"
 
 echo "Finish"
