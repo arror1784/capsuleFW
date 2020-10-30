d6a7b55cd197cf7e0636ef9db1f11971abaf35b3
SeungHwan Jeon <arror1784@gmail.com>
2020-10-30 13:56:47 +0900

    update for update files

--- package/update.sh
+++ package/update.sh
@@ -7,9 +7,15 @@ SERVICES=(
 
 ROOT_UPDATE_TARGET=(
 "capsuleFW"
+"C10.service"
+"libstdc++.so.6.0.26"
+"wpa_supplicant.conf"
 )
 ROOT_UPDATE_PATH=(
 "/opt/capsuleFW/bin/capsuleFW"
+"/etc/avahi/services/C10.service"
+"/usr/lib/arm-linux-gnueabihf/libstdc++.so.6"
+"/etc/wpa_supplicant/wpa_supplicant.conf"
 )
 
 if [ $# -eq 0 ]; then
@@ -18,13 +24,22 @@ if [ $# -eq 0 ]; then
 fi
 TARGET_FILE_NAME=$1
 TARGET_FOLDER_NAME=${TARGET_FILE_NAME%%.*}
-
+CURRENT_VERSION=$(cat "/opt/capsuleFW/version.json" | python3 -c "import sys, json; print(json.load(sys.stdin)['version'])")
 unzip -o $TARGET_FILE_NAME -d $2
 
 for (( i = 0 ; i < ${#SERVICES[@]} ; i++ )) ; do
 	systemctl stop ${SERVICES[$i]}
 done
 
+if [ "$CURRENT_VERSION" == "1.1.1" ]; then
+	echo "1.1.1 libstdc++.so.6 wpa_supplicant"	
+	cp -rf "${TARGET_FOLDER_NAME}/libstdc++.so.6.0.26" "/usr/lib/arm-linux-gnueabihf/libstdc++.so.6"
+	cp -rf ${TARGET_FOLDER_NAME}/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+fi
+
+cp -rf ${TARGET_FOLDER_NAME}/react.service /etc/systemd/system/react.service
+cp -rf ${TARGET_FOLDER_NAME}/C10.service /etc/avahi/services/C10.service
+service avahi-daemon restart
 
 rm -rf /opt/capsuleFW/version.json
 cp -rf $3 /opt/capsuleFW/
