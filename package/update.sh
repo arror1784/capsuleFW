deeb326599123604f03c76e1c3de2d788c9bc172
SeungHwan Jeon <arror1784@gmail.com>
2020-11-15 21:18:42 +0900

    change update script

--- package/update.sh
+++ package/update.sh
@@ -68,7 +68,7 @@ if type yarn 2> /dev/null; then
 else
 	echo "yarn install"
 	curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-	echo \"deb https://dl.yarnpkg.com/debian/ stable main\" | tee /etc/apt/sources.list.d/yarn.list
+	echo "deb https://dl.yarnpkg.com/debian/ stable main\" | tee /etc/apt/sources.list.d/yarn.list"
 
 	apt-get update -y
 	apt-get install yarn -y
