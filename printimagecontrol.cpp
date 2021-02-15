faa7579684847937129a15b3654f5db0a6cbdafb
arror1784 <arror1784@gmail.com>
2021-02-15 16:41:28 +0900

    printImageControl add code

--- printimagecontrol.cpp
+++ printimagecontrol.cpp
@@ -4,3 +4,28 @@ PrintImageControl::PrintImageControl()
 {
 
 }
+void PrintImageControl::imageWrote()
+{
+    _imageWrote = true;
+    _cv_image.notify_all();
+}
+
+void PrintImageControl::imageScale(double scale)
+{
+    emit sendToQmlImageScale(scale);
+}
+
+void PrintImageControl::imageRotate(int rotate)
+{
+    emit sendToQmlImageRotate(rotate);
+}
+
+void PrintImageControl::imageWidhtHeight(int width, int height)
+{
+    emit sendToQmlImageWidhtHeight(width,height);
+}
+void PrintImageControl::waitImageWrote()
+{
+    std::unique_lock<std::mutex> lk(_cv_image_m);
+    _cv_image.wait(lk,[this]{return this->_imageWrote;});
+}
--- printimagecontrol.h
+++ printimagecontrol.h
@@ -1,11 +1,37 @@
 #ifndef PRINTIMAGECONTROL_H
 #define PRINTIMAGECONTROL_H
 
+#include <QObject>
+#include <mutex>
+#include <condition_variable>
 
-class PrintImageControl
+class PrintImageControl : public QObject
 {
+    Q_OBJECT
+
 public:
     PrintImageControl();
+
+signals:
+    void sendToQmlChangeImage(QString path);
+    void sendToQmlImageScale(double scale);
+    void sendToQmlImageRotate(int rotate);
+    void sendToQmlImageWidhtHeight(int width,int height);
+
+public slots:
+    void imageWrote();
+
+public:
+    virtual void imageChange(int number);
+    void imageScale(double scale);
+    void imageRotate(int rotate);
+    void imageWidhtHeight(int width,int height);
+    void waitImageWrote();
+
+private:
+    std::condition_variable _cv_image;
+    std::mutex _cv_image_m;
+    bool _imageWrote = true;
 };
 
 #endif // PRINTIMAGECONTROL_H
